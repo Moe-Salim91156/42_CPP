990f4f70638c42ee267ff468bcb4c2a71287a8ce
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-08-03 19:04:36 +0300

    little edti , trying the last exercise

---
+++ CPP00/ex02/Accounts.cpp
@@ -0,0 +1,85 @@
+#include	"Account.hpp"
+#include <iostream>
+
+int Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int	Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+int	Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+void	Account::_displayTimestamp()
+{
+	
+}
+
+void	Account::displayAccountsInfos()
+{
+	std::cout << "accounts : " << _nbAccounts << ";" << "total :" << _totalAmount << ";" << "deposits : " << _totalNbDeposits << ";"
+		<< "withdrawls : " << _totalNbWithdrawals << ";" << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	int new_amount  = this->_amount + deposit;
+	std::cout << "index :" << this->_accountIndex << ";" <<  "p_amount :" << this->_amount << ";" <<
+		"deposit:" << deposit << ";" << "amount:" << new_amount << ";" << "nb_deposits"<< this->_nbDeposits << std::endl;
+	this->_amount  = new_amount; 
+	this->_nbDeposits++;
+}
+
+bool Account::makeWithdrawal(int withdrawl)
+{
+	std::cout << "index :" << this->_accountIndex << ";" <<  "p_amount :" << this->_amount << ";";	
+	std::cout << "withdrawl:";
+	if (withdrawl > this->_amount)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	//TODO  withdrawl condition on log if good or not , printing differ
+	this->_amount -= withdrawl;
+	this->_nbWithdrawals++;
+	return (true);
+}
+
+int	Account::checkAmount(void) const
+{
+	return (0);
+}
+
+void	Account::displayStatus(void) const
+{
+
+}
+
+Account::Account(int initial_deposit)
+{
+	_amount = initial_deposit;
+	_totalAmount += _amount;
+	_accountIndex = getNbAccounts();
+	_nbAccounts++;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	//logtime display
+}
+
+//destructor
+Account::~Account()
+{
+	//closed
+}
+
