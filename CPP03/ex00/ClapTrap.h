3135c7951191f2929bfbdc5a9130b0df3add1a1f
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-09-02 15:32:04 +0300

    asdf

--- CPP03/ex00/ClapTrap.h
+++ CPP03/ex00/ClapTrap.h
@@ -1,16 +1,15 @@
 #include <iostream>
 
+class ClapTrap
+{
+  private:
+	std::string name;
+	unsigned int _hp;
+	unsigned int _energy_points;
+	unsigned int _attack_dmg;
 
-class ClapTrap {
-	private:
-		std::string name;
-		unsigned int	_hp;
-		unsigned int	_energy_points;
-		unsigned int	_attack_dmg;
-
-	public:
-		void	attack(const std::string& target);
-		void	takeDamage(unsigned int amount);
-		void	beRepaired(unsigned int amount);
+  public:
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 };
-
